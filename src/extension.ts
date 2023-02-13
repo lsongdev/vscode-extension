10854e41211342f8c475e66099631806a3da294e
Lsong <song940@gmail.com>
2023-02-14 00:40:10 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,29 @@
-const vscode = require('vscode');
+import * as vscode from "vscode";
+import fetch from 'node-fetch';
+import { OpenAI, Configuration } from '@song940/openai';
+
+// @ts-ignore
+globalThis.fetch = fetch;
+
+let openai: any;
 
 const { window, commands, StatusBarAlignment } = vscode;
 const { createStatusBarItem, showInformationMessage } = window;
 
-const commandHandler = (command) => {
-  showInformationMessage(command);
-};
-
-const handleSelectedText = command => {
-  const { selection } = vscode.window.activeTextEditor;
+const commandHandler = async (prefix: string) => {
+  const selection = vscode.window.activeTextEditor?.selection;
   const selectedText = window.activeTextEditor?.document.getText(selection);
-  console.debug(command, selectedText);
+  const config = vscode.workspace.getConfiguration('chatgpt');
+  const prompt = config.get(prefix);
+  const searchPrompt = `${prompt}\n\`\`\`\n${selectedText}\n\`\`\``;
+  // showInformationMessage(searchPrompt);
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: searchPrompt,
+    temperature: 0,
+    max_tokens: 2048
+  });
+  console.log(completion.choices[0].text);
 };
 
 const commandHello = commands.registerCommand('extension.sayHello', () => {
@@ -18,24 +31,30 @@ const commandHello = commands.registerCommand('extension.sayHello', () => {
 });
 
 const commandExplain = vscode.commands.registerCommand('chatgpt.explain', () => {
-  handleSelectedText('promptPrefix.explain');
+  commandHandler('promptPrefix.explain');
 });
 
 const commandRefactor = vscode.commands.registerCommand('chatgpt.refactor', () => {
-  handleSelectedText('promptPrefix.refactor');
+  commandHandler('promptPrefix.refactor');
 });
 const commandOptimize = vscode.commands.registerCommand('chatgpt.optimize', () => {
-  handleSelectedText('promptPrefix.optimize');
+  commandHandler('promptPrefix.optimize');
 });
 const commandProblems = vscode.commands.registerCommand('chatgpt.findProblems', () => {
-  handleSelectedText('promptPrefix.findProblems');
+  commandHandler('promptPrefix.findProblems');
 });
 
 // @docs https://code.visualstudio.com/api/references/activation-events
-const activate = context => {
+export const activate = (context: vscode.ExtensionContext) => {
 
   const config = vscode.workspace.getConfiguration('chatgpt');
   console.log('config:', config);
+
+  const configuration = new Configuration({
+    apiKey: config.get('apiKey'),
+  });
+
+  openai = new OpenAI(configuration);
 
   showInformationMessage("Hello from VSCode Extension");
 
@@ -53,11 +72,6 @@ const activate = context => {
   context.subscriptions.push(commandProblems);
 };
 
-const deactivate = () => {
+export const deactivate = () => {
   console.debug('deactive');
-};
-
-module.exports = {
-  activate,
-  deactivate,
 };
